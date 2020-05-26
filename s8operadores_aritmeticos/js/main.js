ba37270f50f45bb87efcb6a0d1f35cb44f506de3
Karen1501 <karencsj0391@gmail.com>
2020-05-26 16:35:33 -0500

    operadores-aritmeticos

---
+++ s8operadores_aritmeticos/js/main.js
@@ -0,0 +1,26 @@
+var a = 10
+var b = 20
+
+var adittionResult = a + b
+console.log("La suma de A más B es igual a ", adittionResult)
+
+var c = 12
+var d = 22
+
+var substractionResult = c + d
+
+console.log("La resta de C - D es igual a ", substractionResult)
+
+var x = 45
+var y = 9
+
+var divisionResult = x / y
+
+console.log("La división de X entre Y es igual a ", divisionResult)
+
+var e = 65
+var f = 8
+
+var remainderResult = 65 % 8
+
+console.log("El residuo de 65 entre 8 es ", remainderResult)
