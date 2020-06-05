6219eed5d69b6a8af036603f01006fa2ca79f8de
Karen1501 <karencsj0391@gmail.com>
2020-06-04 19:15:12 -0500

    practica11

---
+++ practica11/js/main.js
@@ -0,0 +1,110 @@
+/*
+    crear una función que pida al usuario los siguientes datos:
+    nombre, apellidos, calle, número, colonia, código postal, alcaldía o municipio
+    esa función debe devolver un objeto con la siguiente estructura: 
+    {
+        nombre:
+        apellidos:
+        direccion:{
+            calle:
+            numero:
+            colonia:
+            cp:
+            alcaldia:
+        }
+    }
+    
+*/
+const createUserObject = () => {
+
+    var userInfo = {}
+
+    var name = prompt("Ingresa tu nombre")
+    var lastName = prompt("Ingresa tus apellidos")
+    var adress = {}
+    var street = prompt("Ingresa el nombre de la calle")
+    var number = parseInt(prompt("Ingresa el número"))
+    var colony = prompt("Ingresa la colonia ")
+    var cp = parseInt(prompt("Ingresa tu código postal"))
+    var townHall = prompt("Ingresa la alcadía")
+
+
+    userInfo.name = name,
+        userInfo.lastName = lastName,
+        userInfo.adress = {}
+    userInfo.adress.street = street,
+        userInfo.adress.number = number,
+        userInfo.adress.colony = colony,
+        userInfo.adress.cp = cp,
+        userInfo.adress.townHall = townHall
+    return userInfo
+}
+
+
+/* crear una función que pida al usuario un año (2000, 1989, etc).
+la función debe extraer los autos del array cars, cuyo año sea mayor al indicado por el usuario
+por cada auto extraído, mostrar al usuario un mensaje que diga 
+"Opción ${indice}: ${fabricante del auto} ${nombre del auto} del año ${año de fabricación}, con ${hp} caballos de fuerza"*/
+
+
+
+
+
+
+var cars = [{
+        year: "2000",
+        maker: "Toyota",
+        model: "Corolla",
+        hp: "300"
+    },
+    {
+        year: "1967",
+        maker: "Ford",
+        model: "Mustang",
+        hp: "500"
+    },
+    {
+        year: "2000",
+        maker: "Nissan",
+        model: "Sentra",
+        hp: "128"
+    },
+    {
+        year: "2012",
+        maker: "Mitubishi",
+        model: "Lancer",
+        hp: "300"
+    },
+    {
+        year: "2000",
+        maker: "Nissan",
+        model: "Altima",
+        hp: "200"
+    },
+    {
+        year: "1990",
+        maker: "Volkswagen",
+        model: "Sedan",
+        hp: "80"
+    },
+    {
+        year: "2015",
+        maker: "Nissan",
+        model: "Tiida",
+        hp: "200"
+    }
+]
+
+
+
+const carsFiltered = () => {
+        var year = prompt("Escribe un año")
+        var filter = cars.filter(car => cars.year < year)
+        return filter
+
+
+        var autos = cars.map(function(car, index) {
+            console.log(index + " " + "fabricante del auto" + " " + car.maker + " " + "nombre del auto" + " " + car.model + " " + "del año" + " " + car.year + " " + "con" + " " + car.hp + " " + "caballos de fuerza")
+
+        })
+    } //después de esto, me atoré
